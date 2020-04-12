ce6f6165bd335325c55554a9a1bde51a2a9f4d01
andrewcmk <andrewcmk97@gmail.com>
2020-04-12 20:58:07 +0100

    made changes

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("connection secured");
+
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
